8e5f9598d43493f67eb85de05e7eb913e75e5035
Richard Evans <rickecon@gmail.com>
2023-10-04 01:15:55 -0600

    Formated files

--- docs/book/conf.py
+++ docs/book/conf.py
@@ -3,7 +3,9 @@
 html_static_path = ["./_static"]
 
 # CSS files
-html_css_files = ["custom.css",]
+html_css_files = [
+    "custom.css",
+]
 
 # Get rid of transitions error/warning
 myst_footnote_transition = False
