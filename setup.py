2967598ac1c9ac60208365acbf13c02d972de407
Richard Evans <rickecon@gmail.com>
2023-10-27 04:31:56 -0600

    Updated the version in setup.py and CHANGELOG.md

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as readme_file:
 
 setup(
     name="CompMethods",
-    version="0.0.4",
+    version="0.0.5",
     author="Richard W. Evans",
     author_email="rickecon@gmail.com",
     long_description=readme,
