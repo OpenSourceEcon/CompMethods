45a0f03269c46e00bd71ced0fe63eb71a3a7b7af
Richard Evans <rickecon@gmail.com>
2023-12-15 16:25:14 -0700

    Black formatted two files

--- code/gmm/distributions.py
+++ code/gmm/distributions.py
@@ -1,4 +1,4 @@
-'''
+"""
 ------------------------------------------------------------------------
 This module contains the functions for probability density functions of
 continuous PDF's.
@@ -8,21 +8,21 @@ This Python module defines the following function(s):
     GG_pdf()
     GB2_pdf()
 ------------------------------------------------------------------------
-'''
+"""
 # Import packages
 import numpy as np
 import scipy.special as spc
 
 
-'''
+"""
 ------------------------------------------------------------------------
     Functions
 ------------------------------------------------------------------------
-'''
+"""
 
 
 def LN_pdf(xvals, mu, sigma):
-    '''
+    """
     --------------------------------------------------------------------
     This function gives the PDF of the lognormal distribution for xvals
     given mu and sigma
@@ -46,16 +46,19 @@ def LN_pdf(xvals, mu, sigma):
 
     RETURNS: pdf_vals
     --------------------------------------------------------------------
-    '''
-    pdf_vals = np.float64(((1 / (np.sqrt(2 * np.pi) * sigma * xvals)) *
-                          np.exp((-1.0 / 2.0) *
-                          (((np.log(xvals) - mu) / sigma) ** 2))))
+    """
+    pdf_vals = np.float64(
+        (
+            (1 / (np.sqrt(2 * np.pi) * sigma * xvals))
+            * np.exp((-1.0 / 2.0) * (((np.log(xvals) - mu) / sigma) ** 2))
+        )
+    )
 
     return pdf_vals
 
 
 def GA_pdf(xvals, alpha, beta):
-    '''
+    """
     --------------------------------------------------------------------
     Returns the PDF values from the two-parameter gamma (GA)
     distribution. See McDonald and Xu (1995).
@@ -80,16 +83,18 @@ def GA_pdf(xvals, alpha, beta):
 
     RETURNS: pdf_vals
     --------------------------------------------------------------------
-    '''
-    pdf_vals = \
-        np.float64((1 / ((beta ** alpha) * spc.gamma(alpha))) *
-                   (xvals ** (alpha - 1)) * np.exp(-xvals / beta))
+    """
+    pdf_vals = np.float64(
+        (1 / ((beta**alpha) * spc.gamma(alpha)))
+        * (xvals ** (alpha - 1))
+        * np.exp(-xvals / beta)
+    )
 
     return pdf_vals
 
 
 def GG_pdf(xvals, alpha, beta, mm):
-    '''
+    """
     --------------------------------------------------------------------
     Returns the PDF values from the three-parameter generalized gamma
     (GG) distribution. See McDonald and Xu (1995).
@@ -118,17 +123,18 @@ def GG_pdf(xvals, alpha, beta, mm):
 
     RETURNS: pdf_vals
     --------------------------------------------------------------------
-    '''
-    pdf_vals = \
-        np.float64((mm / ((beta ** alpha) * spc.gamma(alpha / mm))) *
-                   (xvals ** (alpha - 1)) *
-                   np.exp(-((xvals / beta) ** mm)))
+    """
+    pdf_vals = np.float64(
+        (mm / ((beta**alpha) * spc.gamma(alpha / mm)))
+        * (xvals ** (alpha - 1))
+        * np.exp(-((xvals / beta) ** mm))
+    )
 
     return pdf_vals
 
 
 def GB2_pdf(xvals, aa, bb, pp, qq):
-    '''
+    """
     --------------------------------------------------------------------
     Returns the PDF values from the four-parameter generalized beta 2
     (GB2) distribution. See McDonald and Xu (1995).
@@ -158,10 +164,14 @@ def GB2_pdf(xvals, aa, bb, pp, qq):
 
     RETURNS: pdf_vals
     --------------------------------------------------------------------
-    '''
-    pdf_vals = \
-        np.float64((aa * (xvals ** (aa * pp - 1))) / ((bb ** (aa * pp)) *
-                   spc.beta(pp, qq) *
-                   ((1 + ((xvals / bb) ** aa)) ** (pp + qq))))
+    """
+    pdf_vals = np.float64(
+        (aa * (xvals ** (aa * pp - 1)))
+        / (
+            (bb ** (aa * pp))
+            * spc.beta(pp, qq)
+            * ((1 + ((xvals / bb) ** aa)) ** (pp + qq))
+        )
+    )
 
     return pdf_vals
